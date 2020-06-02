053ce9ac28ef978ad53f0bd7c066530f3544da6d
William Tambe <tambewilliam@gmail.com>
2020-06-02 03:47:44 -0500

    Target PU32 support

--- package/gdb/gdb.mk
+++ package/gdb/gdb.mk
@@ -14,6 +14,13 @@ GDB_SOURCE = gdb-$(GDB_VERSION).tar.gz
 GDB_FROM_GIT = y
 endif
 
+ifeq ($(BR2_pu32),y)
+GDB_VERSION = pu32.20220920
+GDB_SITE = $(call github,fontamsoc,binutils-gdb,$(GDB_VERSION))
+GDB_SOURCE = $(GDB_VERSION).tar.gz
+GDB_FROM_GIT = y
+endif
+
 GDB_LICENSE = GPL-2.0+, LGPL-2.0+, GPL-3.0+, LGPL-3.0+
 GDB_LICENSE_FILES = COPYING COPYING.LIB COPYING3 COPYING3.LIB
 GDB_CPE_ID_VENDOR = gnu
